15175abc575b901ec2c69c594477988029cb35c8
Paco Portela <pacoportela@yahoo.com>
2024-10-07 19:45:28 +0200

    Terminado.

---
+++ OneDrive/Documents/NetBeansProjects/UtilidadesElco/src/utilidadeselco/UtilidadesElcoApp.java
@@ -0,0 +1,44 @@
+/*
+ * UtilidadesElcoApp.java
+ */
+
+package utilidadeselco;
+
+import org.jdesktop.application.Application;
+import org.jdesktop.application.SingleFrameApplication;
+
+/**
+ * The main class of the application.
+ */
+public class UtilidadesElcoApp extends SingleFrameApplication {
+
+    /**
+     * At startup create and show the main frame of the application.
+     */
+    @Override protected void startup() {
+        show(new UtilidadesElcoView(this));
+    }
+
+    /**
+     * This method is to initialize the specified window by injecting resources.
+     * Windows shown in our application come fully initialized from the GUI
+     * builder, so this additional configuration is not needed.
+     */
+    @Override protected void configureWindow(java.awt.Window root) {
+    }
+
+    /**
+     * A convenient static getter for the application instance.
+     * @return the instance of UtilidadesElcoApp
+     */
+    public static UtilidadesElcoApp getApplication() {
+        return Application.getInstance(UtilidadesElcoApp.class);
+    }
+
+    /**
+     * Main method launching the application.
+     */
+    public static void main(String[] args) {
+        launch(UtilidadesElcoApp.class, args);
+    }
+}
